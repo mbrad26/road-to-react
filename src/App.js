d6e81f94a16fc999b9b8025fc028f6409844ad6d
mbrad26 <mbrad26@gmail.com>
2020-07-18 12:14:14 +0100

    extracted a Search componnent

--- src/App.js
+++ src/App.js
@@ -20,21 +20,9 @@ function App() {
   return (
     <div>
       <h1>Hello World!</h1>
-      <label htmlFor='search'>Search: </label>
-      <input id='search' type='text' />
+      <Search />
       <hr />
-      {list.map((item, index) => {
-        return (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        )
-      })}
+      <List />
     </div>
   );
 }
@@ -52,6 +40,15 @@ function List() {
       </div>
     );
   });
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
+    </div>
+  );
 }
 
 export default App;
