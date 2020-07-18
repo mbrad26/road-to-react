e426096815b1f40698f93d17d30e4d65b4c7b64a
mbrad26 <mbrad26@gmail.com>
2020-07-18 17:07:01 +0100

    refactored

--- src/App.js
+++ src/App.js
@@ -51,17 +51,18 @@ const Search = ({ search, onSearch }) => (
     </div>
   )
 
+const List = ({ list }) =>
+  list.map(({ objectID, ...item }) => <Item key={objectID} {...item} />);
 
-const List = (props) =>
-  props.list.map(item => (
-    <div key={item.objectID}>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-    </div>
-  ));
+const Item = ({ title, url, author, num_comments, points }) => (
+  <div>
+    <span>
+      <a href={url}>{title}</a>
+    </span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
+  </div>
+);
 
 export default App;
