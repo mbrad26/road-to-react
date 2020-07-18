d809be07ede5c85f8819200b2c4463a7dc92e8ff
mbrad26 <mbrad26@gmail.com>
2020-07-18 11:59:08 +0100

    renered a list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import './App.css';
 
+const list = [
+    {
+  title: 'React',
+  url: 'https://reactjs.org/', author: 'Jordan Walke', num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: 'Redux',
+  url: 'https://redux.js.org/', author: 'Dan Abramov, Andrew Clark', num_comments: 2,
+  points: 5,
+  objectID: 1,
+  },
+];
+
 function App() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
+      <hr />
+      {list.map((item, index) => {
+        return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        )
+      })}
     </div>
   );
 }
