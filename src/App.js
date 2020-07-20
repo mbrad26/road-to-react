4c9eed067d3b1a069ade9072fc649bee848dda8c
mbrad26 <mbrad26@gmail.com>
2020-07-20 13:27:44 +0100

    added css to App and Item component

--- src/App.js
+++ src/App.js
@@ -97,14 +97,13 @@ const App = () => {
   );
 
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className='container'>
+      <h1 className='headline-promary'>My Hacker Stories</h1>
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
       />
-      <hr />
 
       {stories.isError && <p>Something went wrong ...</p>}
 
@@ -142,7 +141,6 @@ const SearchForm = ({
       </button>
     </form>
   )
-
 
 const InputWithLabel = ({
     id,
@@ -190,15 +188,16 @@ const Item = ({ item, onRemoveItem }) => {
     onRemoveItem(item);
 
   return (
-    <div>
-      <span>
+    <div className='item'>
+      <span style={{ width: '40%' }}>
         <a href={item.url}>{item.title}</a>
       </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-      <span>
+      <span style={{ width: '30%' }}>{item.author}</span>
+      <span style={{ width: '10%' }}>{item.num_comments}</span>
+      <span style={{ width: '10%' }}>{item.points}</span>
+      <span style={{ width: '10%' }}>
         <button
+          className='button button_small'
           type='button'
           onClick={() => onRemoveItem(item)}
         >
