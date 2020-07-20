409f128c12edd2349f93ccead32e19e4ab0a4a6c
mbrad26 <mbrad26@gmail.com>
2020-07-20 09:10:02 +0100

    replaced fetch with axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef, useReducer, useCallback } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const useSemiPersistentState = (key, initialState) => {
@@ -58,12 +59,12 @@ const App = () => {
     if (!searchTerm) return;
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(url)
-      .then(response => response.json())
+    axios
+      .get(url)
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() =>
