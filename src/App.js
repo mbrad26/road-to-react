f611f69916cd9419776a29ba2acbadbd7802f2a2
mbrad26 <mbrad26@gmail.com>
2020-07-21 10:58:18 +0100

    Item componnent renders all the expected properties

--- src/App.js
+++ src/App.js
@@ -211,30 +211,25 @@ const List = React.memo(({ list, onRemoveItem }) =>
   ))
 );
 
-const Item = ({ item, onRemoveItem }) => {
-  const handleRemoveItem = () =>
-    onRemoveItem(item);
-
-  return (
-    <div className='item'>
-      <span style={{ width: '40%' }}>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span style={{ width: '30%' }}>{item.author}</span>
-      <span style={{ width: '10%' }}>{item.num_comments}</span>
-      <span style={{ width: '10%' }}>{item.points}</span>
-      <span style={{ width: '10%' }}>
-        <button
-          className='button button_small'
-          type='button'
-          onClick={() => onRemoveItem(item)}
-        >
-          <Check height='18px' width='18px' />
-        </button>
-      </span>
-    </div>
-  )
-};
+const Item = ({ item, onRemoveItem }) => (
+  <div className='item'>
+    <span style={{ width: '40%' }}>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span style={{ width: '30%' }}>{item.author}</span>
+    <span style={{ width: '10%' }}>{item.num_comments}</span>
+    <span style={{ width: '10%' }}>{item.points}</span>
+    <span style={{ width: '10%' }}>
+      <button
+        className='button button_small'
+        type='button'
+        onClick={() => onRemoveItem(item)}
+      >
+        <Check height='18px' width='18px' />
+      </button>
+    </span>
+  </div>
+);
 
 export default App;
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,73 +1,86 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {
+  render,
+  screen,
+  fireEvent,
+  act,
+} from '@testing-library/react';
 import App, {
   storiesReducer,
-  Item,
   List,
+  Item,
   SearchForm,
   InputWithLabel,
 } from './App';
 
-describe('App', () => {
-  const storyOne = {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  }
+const storyOne = {
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+}
 
-  const storyTwo = {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
+const storyTwo = {
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+}
 
-  const stories = [storyOne, storyTwo];
+const stories = [storyOne, storyTwo];
 
-  describe('storiesReducer', () => {
-    it('removes a story from all stories', () => {
-      const state = { data: stories, isLoading: false, isError: false };
-      const action = { type: 'REMOVE_STORY', payload: storyOne };
+describe('storiesReducer', () => {
+  it('removes a story from all stories', () => {
+    const state = { data: stories, isLoading: false, isError: false };
+    const action = { type: 'REMOVE_STORY', payload: storyOne };
 
-      const newState = storiesReducer(state, action);
-      const expectedState = { data: [storyTwo], isLoading: false, isError: false};
+    const newState = storiesReducer(state, action);
+    const expectedState = { data: [storyTwo], isLoading: false, isError: false};
 
-      expect(newState).toStrictEqual(expectedState);
-    });
+    expect(newState).toStrictEqual(expectedState);
+  });
 
-    it('isLoading = true', () => {
-      const state = { isLoading: false, isError: false};
-      const action = { type: 'STORIES_FETCH_INIT' };
+  it('isLoading = true', () => {
+    const state = { isLoading: false, isError: false};
+    const action = { type: 'STORIES_FETCH_INIT' };
 
-      const newState = storiesReducer(state, action);
-      const expectedState = { isLoading: true, isError: false}
+    const newState = storiesReducer(state, action);
+    const expectedState = { isLoading: true, isError: false}
 
-      expect(newState).toStrictEqual(expectedState)
-    });
+    expect(newState).toStrictEqual(expectedState)
+  });
 
-    it('fetches stories', () => {
-      const state = { isLoading: true, isError: false };
-      const action = { type: 'STORIES_FETCH_SUCCESS', payload: stories };
+  it('fetches stories', () => {
+    const state = { isLoading: true, isError: false };
+    const action = { type: 'STORIES_FETCH_SUCCESS', payload: stories };
 
-      const newState = storiesReducer(state, action);
-      const expectedState = { data: stories, isLoading: false, isError: false };
+    const newState = storiesReducer(state, action);
+    const expectedState = { data: stories, isLoading: false, isError: false };
 
-      expect(newState).toStrictEqual(expectedState);
-    });
+    expect(newState).toStrictEqual(expectedState);
+  });
 
-    it('fails to fetch stories', () => {
-      const state = { isLoading: true, isError: false };
-      const action = { type: 'STORIES_FETCH_FAILURE' };
+  it('fails to fetch stories', () => {
+    const state = { isLoading: true, isError: false };
+    const action = { type: 'STORIES_FETCH_FAILURE' };
 
-      const newState = storiesReducer(state, action);
-      const expectedState = { isLoading: false, isError: true };
+    const newState = storiesReducer(state, action);
+    const expectedState = { isLoading: false, isError: true };
 
-      expect(newState).toStrictEqual(expectedState);
-    });
+    expect(newState).toStrictEqual(expectedState);
+  });
+});
+
+describe('Item', () => {
+  it('renders all properties', () => {
+    render(<Item item={storyOne} />);
+
+    expect(screen.getByText('Jordan Walke')).toBeInTheDocument();
+    expect(screen.getByText('React')).toBeInTheDocument();
+    screen.debug();
   });
 });
